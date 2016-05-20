145e2832aacf8b1147d568597fc01bcbddf38385
chrisyhsun <chrisyhsun@utexas.edu>
2016-05-20 01:12:14 -0500

    fill works correctly, now passing null tests

--- NLG/src/FrameFunctions.py
+++ NLG/src/FrameFunctions.py
@@ -4,7 +4,10 @@ Location = r'C:\CreditSuisse\NaturalLanguageGenerator\locus_data_12years.csv'
 df = pd.read_csv(Location)
 
 df_index = df.set_index('Date')
-
+print(df_index.irow(912))
+df_index.fillna(method='bfill', inplace=True)
+print('     AFTER')
+print(df_index.irow(912))
 
 def get_delta(ticker, first_date, second_date):
     return bp_format(df_index.at[second_date, ticker] - df_index.at[first_date, ticker])
--- NLG/src/Tests.py
+++ NLG/src/Tests.py
@@ -8,21 +8,36 @@ class Tests(unittest.TestCase):
     def test_daycompare(self):
         # tests for 2yr 10yr diff
         self.assertEqual(NLG.day_compare('2yr-10yr_UST', '09-Jun-05', '10-Jun-05'),
-                         'The difference between 2yr and 10yr yield steepened by 1bps.')
+                    'The difference between 2yr and 10yr yield steepened by 1bps.')
         self.assertEqual(NLG.day_compare('2yr-10yr_UST', '14-Nov-05', '15-Nov-05'),
-                         'The difference between 2yr and 10yr yield flattened by 3bps.')
+                    'The difference between 2yr and 10yr yield flattened by 3bps.')
         print('done with 2yr 10yr diff tests')
         # tests for Treasury rates
         self.assertEqual(NLG.day_compare('10yr_tsy', '19-May-05', '20-May-05'),
-                         'The 10 year Treasury rates sold off by 2bps.')
+                    'The 10 year Treasury rates sold off by 2bps.')
         self.assertEqual(NLG.day_compare('10yr_tsy', '25-Jan-10', '26-Jan-10'),
-                         'The 10 year Treasury rates rallied by 0bps.')
+                    'There was no change in the 10 year Treasury rates.')
         print('done with Treasury rates tests')
         # tests for swap rates
+        self.assertEqual(NLG.day_compare('5yr_swap', '03-Jul-07', '05-Jul-07'),
+                    'The 5 year swap rates sold off by 10bps.')
+        self.assertEqual(NLG.day_compare('5yr_swap', '02-Jul-10', '06-Jul-10'),
+                    'The 5 year swap rates rallied by 5bps.')
         print('done with swap rates tests')
         # tests for mortgages
+        # both null compare
+        self.assertEqual(NLG.day_compare('30_YR_Survey_Rate', '18-May-05', '19-May-05'),
+                    'There was no change in the 30 year mortgage rate.')
+        # one null compare
+        self.assertEqual(NLG.day_compare('30_YR_Survey_Rate', '07-Jan-09', '08-Jan-09'),
+                    'The 30 year mortgage rate sold off by 3bps.')
+        # both available compare
+        self.assertEqual(NLG.day_compare('30_YR_Survey_Rate', '23-Feb-09', '24-Feb-09'),
+                    'The 30 year mortgage rate rallied by 2bps.')
+        self.assertEqual(NLG.day_compare('30_YR_Survey_Rate', '23-May-11', '24-May-11'),
+                    'The 30 year mortgage rate sold off by 7bps.')
         print('done with mortgage tests')
-        # tests for spreads
+        # tests for spreads TODO should be a 2 difference thing!!
         print('done with spreads tests')
 
 if __name__ == '__main__':
