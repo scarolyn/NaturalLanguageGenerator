24206bd2cc1993cae27b24a410133295bdf6b766
scarolyn <carolyn.tran@outlook.com>
2016-05-20 04:53:05 -0500

    everything

--- NLG/src/FrameFunctions.py
+++ NLG/src/FrameFunctions.py
@@ -1,13 +1,20 @@
 import pandas as pd
+from scipy import stats
+import sys
 
 Location = r'C:\Users\Carolyn\workspace\NaturalLanguageGenerator\locus_data_12years.csv'
 df = pd.read_csv(Location)
 
 df_index = df.set_index('Date')
+df_index.fillna(method='bfill')
 
 
 def get_delta(ticker, first_date, second_date):
-    return bp_format(df_index.at[second_date, ticker] - df_index.at[first_date, ticker])
+    try:
+        return bp_format(df_index.at[second_date, ticker] - df_index.at[first_date, ticker])
+    except KeyError:
+        print('Could not find dates.')
+        sys.exit()
 
 
 def get_value(ticker, date):
@@ -20,3 +27,47 @@ def bp_format(number):
 
 def get_headers():
     return list(df_index.columns.values)
+
+
+def get_linregress(name, date1, date2):
+    series = df_index.loc[:, name : name].values[get_row_index(date1):get_row_index(date2)]
+    series = [float(x) for x in series]
+    dates = df.index.values[get_row_index(date1):get_row_index(date2)]
+    slope, intercept, r_val, p_val, std_err = stats.linregress(dates,series)
+    return p_val, slope
+
+
+def get_row_index(date):
+    count = -1
+    for x in df['Date'].tolist():
+        count += 1
+        if x == date:
+            return count
+    return -1
+
+
+def get_col_index(ticker):
+    count = -1
+    for x in get_headers():
+        count += 1
+        if x == ticker:
+            return count
+
+# create a sub df from the beginning to curDate
+def get_lowest(name, date):
+    # series = df_index.loc[0:get_row_index(curDate), name:name].values()
+    # print (series)
+    series = df_index.loc[:, name: name].values[0:get_row_index(date)]
+    min = series[0]
+    count = 0
+    counter = -1
+    for x in series:
+        counter += 1
+        if min > x:
+            min = x
+            count = counter
+
+    return float(min), count
+
+
+
---
+++ NLG/src/flask.py
@@ -0,0 +1,34 @@
+# from flask import Flask
+# from flask import request
+# from flask import render_template
+import Flask
+import NLG as nlg
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return Flask.render_template("Page.html")
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+    if Flask.request.method == 'POST':
+        d1 = Flask.request.form['date1']
+        d2 = Flask.request.form['date2']
+
+        year1 = d1[0:4]
+        month1 = months[int(d1[5:7]) - 1]
+        day1 = d1[8:]
+        date1 = day1 + '-' + month1 + '-' + year1
+
+        year2 = d2[0:4]
+        month2 = months[int(d2[5:7]) - 1]
+        day2 = d2[8:]
+        date2 = day2 + '-' + month2 + '-' + year2
+
+        return nlg.paragraph(date1, date2)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
